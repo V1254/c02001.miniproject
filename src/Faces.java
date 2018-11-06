ed28a30428eb07607fff080cff0ded93cb305dc7
Mohamed <dtmohamed95@gmail.com>
2018-11-06 16:43:41 +0000

    DiceFaces Enum

---
+++ src/Faces.java
@@ -0,0 +1,17 @@
+// Number of Faces on the Dice
+public enum Faces {
+    SIX(6),
+    EIGHT(8),
+    Ten(10),
+    TWENTY(20);
+
+    private int numberOfSides;
+
+    Faces(int x){
+        numberOfSides = x;
+    }
+
+    public int getNumberOfSides() {
+        return numberOfSides;
+    }
+}
