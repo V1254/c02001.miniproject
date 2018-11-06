f9e3191f6d6187534c41433365022496dfbc0d91
Mohamed <dtmohamed95@gmail.com>
2018-11-06 16:42:18 +0000

    Player Class

---
+++ src/Player.java
@@ -0,0 +1,39 @@
+public class Player {
+    private String name;
+    private int score;
+    private boolean turn;
+
+    public Player(){
+        this("Anonymous");
+    }
+
+    public Player(String name){
+        this.name = name;
+        this.score = 0;
+        this.turn = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean isTurn() {
+        return turn;
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
+}
