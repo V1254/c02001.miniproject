0092e3700f91b92d9ada82091e79b9162a0b40c7
Mohamed <dtmohamed95@gmail.com>
2018-11-15 02:02:37 +0000

    removed comments and small styling update

--- src/Controllers/Main.java
+++ src/Controllers/Main.java
@@ -17,22 +17,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-//        loadScenes();
-
         Parent root = FXMLLoader.load(getClass().getResource("/Screens/main.fxml"));
-        Scene scene = new Scene(root,600,500);
+        Scene scene = new Scene(root,600,400);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Dice Game");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/Images/icon.jpg")));
         primaryStage.show();
     }
-
-//    private void loadScenes() throws Exception {
-//        main = FXMLLoader.load(getClass().getResource("/Screens/main.fxml"));
-//        playerScreen = FXMLLoader.load(getClass().getResource("/Screens/playerScreen.fxml"));
-//        twoPlayerScreen = FXMLLoader.load(getClass().getResource("/Screens/twoPlayerScreen.fxml"));
-//    }
 
     public static void main(String[] args) {
         Application.launch(args);
